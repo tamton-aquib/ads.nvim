ead149e14d80c2909ac311783fdb27c993265812
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2026-07-25 01:25:42 +0200

    Add Ratatui ad

--- lua/ads/ads.lua
+++ lua/ads/ads.lua
@@ -4,6 +4,6 @@ return {
     "https://www.smartsheet.com/sites/default/files/styles/1300px/public/2023-09/IC-mcdonalds-im-lovin-it-c.webp?itok=vuORMg59",
     "https://www.realbenidorm.net/assets/images/jet2holidays-experience.jpg",
     "https://www.venclikovi.com/wp-content/uploads/2025/10/NordVPN-Black-Friday-2025-1240x827.webp",
-    "https://i.postimg.cc/qv2VfHCH/emacs-ads.jpg"
+    "https://i.postimg.cc/qv2VfHCH/emacs-ads.jpg",
+    "https://raw.githubusercontent.com/ratatui/ratatui/images/widget-examples/logo.gif"
 }
-
